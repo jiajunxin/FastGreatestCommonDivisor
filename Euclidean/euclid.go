0186ce83b94e528210fc5009fbec7c10708ef8a1
jiajun <jiajun_xin@outlook.com>
2020-11-29 14:10:24 +0800

    optimize euclidean

--- Euclidean/euclid.go
+++ Euclidean/euclid.go
@@ -5,13 +5,13 @@ import (
 )
 
 var zero = big.NewInt(0)
+var two = big.NewInt(2)
 
 // Gcd calculates the greatest common divisor of a and b using Euclidean algorithm
 func Gcd(a, b *big.Int) big.Int {
 	if a.Sign() == 0 {
 		if b.Sign() == 0 {
 			//gcd(0, 0) = 0
-			//fmt.Println("test 0")
 			return *zero
 		}
 		// gcd(0, b) = |b|
@@ -22,6 +22,13 @@ func Gcd(a, b *big.Int) big.Int {
 		// gcd(a, 0) = |a|
 		var ret big.Int
 		return *ret.Abs(a)
+	}
+
+	for isModTwoZero(a) && !isZero(a) {
+		a.Rsh(a, 1)
+	}
+	for isModTwoZero(b) && !isZero(b) {
+		b.Rsh(b, 1)
 	}
 
 	var aTemp, bTemp big.Int
@@ -47,4 +54,22 @@ func gcd(a, b *big.Int) big.Int {
 		return *b
 	}
 	return gcd(b, &reminder)
+}
+
+// isZero returns true if input = 0
+func isZero(input *big.Int) bool {
+	if input.Cmp(zero) == 0 {
+		return true
+	}
+	return false
+}
+
+// isModTwoZero returns true if input mod 2 = 0
+func isModTwoZero(input *big.Int) bool {
+	z := big.NewInt(1)
+	z.Mod(input, two)
+	if isZero(z) {
+		return true
+	}
+	return false
 }
