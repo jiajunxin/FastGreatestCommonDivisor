18ef143d994c991695bf170760f24fe1d2ca1fbd
Jiajun_XIN <jiajun_xin@outlook.com>
2022-07-10 14:15:25 +0800

    experiments for the gcd and factors

--- main.go
+++ main.go
@@ -30,19 +30,41 @@ func main() {
 	fmt.Println("Hello Greatest common divisor")
 	// main function is used to test some large test numbers. Golang benchmark framework can run
 	// limited time.
-	testNum := 100000000
+	testNum := 10000
 
 	rng := rand.New(rand.NewSource(seed))
-	var a, b big.Int
+	var one = big.NewInt(1)
+	var two = big.NewInt(2)
+	var a, b, temp big.Int
+	var gcd = big.NewInt(1)
 	var bufInt = big.NewInt(1)
 
 	start := time.Now()
+	a.Rand(rng, randNumRange)
+	temp.Mod(&a, two)
+	if temp.Cmp(one) != 0 {
+		a.Sub(&a, one)
+	}
+
 	for i := 0; i < testNum; i++ {
 		printProgress(i, testNum)
-		a.Rand(rng, randNumRange)
+
 		b.Rand(rng, randNumRange)
-		_ = bufInt.GCD(nil, nil, &a, &b)
+		// Make sure b is odd
+		temp.Mod(&b, two)
+		if temp.Cmp(one) != 0 {
+			b.Sub(&b, one)
+		}
+		bufInt.GCD(nil, nil, &a, &b)
+		if bufInt.Cmp(one) != 0 {
+			fmt.Println("Found one factor = ", bufInt.String())
+			gcd.Mul(gcd, bufInt)
+			a.Div(&a, bufInt)
+		}
 	}
+
 	elapsed := time.Since(start)
+	fmt.Println("Bit length of a = ", a.String())
 	fmt.Println("Time elapsed = ", elapsed)
+	fmt.Println("Bitlength of gcd = ", gcd.BitLen())
 }
