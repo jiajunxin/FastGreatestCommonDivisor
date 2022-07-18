df62a369a177652b941dd24b22f535741f402ae5
Jiajun_XIN <jiajun_xin@outlook.com>
2022-07-18 16:18:06 +0800

    add experiments for random number difference

--- main.go
+++ main.go
@@ -112,19 +112,57 @@ func WriteFile(input int) {
 		panic(err1)
 	}
 }
+func WriteFileString(input string) {
+	//C:\Users\jxin\OneDrive - HKUST Connect
+	var wireteString = fmt.Sprint(input) + "\n"
+	var filename = "C:/Users/jxin/OneDrive - HKUST Connect/GcdTestResult.txt"
+	var f *os.File
+	var err1 error
+	if checkFileIsExist(filename) {
+		f, err1 = os.OpenFile(filename, os.O_APPEND, 0666)
+	} else {
+		f, err1 = os.Create(filename)
+	}
+	if err1 != nil {
+		panic(err1)
+	}
+	defer f.Close()
+	_, err1 = io.WriteString(f, wireteString)
+	if err1 != nil {
+		panic(err1)
+	}
+}
+
+// TestDifference is used to have a simple test to see the distribution of the difference of two random number
+func TestDifference() {
+	rnd := rand.New(rand.NewSource(999))
+	var a, b, difference big.Int
+	n := big.NewInt(100)
+
+	for i := 0; i < 10000; i++ {
+		printProgress(i, 10000)
+		a.Rand(rnd, n)
+		b.Rand(rnd, n)
+		difference.Sub(&a, &b)
+		difference.Abs(&difference)
+		WriteFileString(difference.String())
+	}
+}
 
 func main() {
 	fmt.Println("Hello Greatest common divisor")
 	// main function is used to test some large test numbers. Golang benchmark framework can run
 	// limited time.
-	round := 1000
-	rng := rand.New(rand.NewSource(9999))
-	totalLength := 0
-	for i := 0; i < round; i++ {
-		printProgress(i, round)
-		temp := GetGcdLen(rng)
-		WriteFile(temp)
-		totalLength += temp
-	}
-	fmt.Println("Average Bitlength of gcd = ", totalLength/round)
+	// round := 10000
+	// rng := rand.New(rand.NewSource(999))
+	// totalLength := 0
+	// for i := 0; i < round; i++ {
+	// 	printProgress(i, round)
+	// 	temp := GetGcdLen(rng)
+	// 	WriteFile(temp)
+	// 	totalLength += temp
+	// }
+	// fmt.Println("Average Bitlength of gcd = ", totalLength/round)
+
+	TestDifference()
 }
